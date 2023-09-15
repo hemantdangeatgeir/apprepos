42b6a303478185e7442f0a80a047f3a68d00a0de
hemantdangeatgeir <hemant.dange@atgeirsolutions.com>
2023-09-15 18:19:23 +0530

    test purpose

--- src/main/java/com/clinicalpatient/intelligence/QueryBuilderContrlr.java
+++ src/main/java/com/clinicalpatient/intelligence/QueryBuilderContrlr.java
@@ -1,5 +1,6 @@
 package com.clinicalpatient.intelligence;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class QueryBuilderContrlr {
 
-	@RequestMapping(value="/getQueryCatalog")
+	@GetMapping("/getQueryCatalog")
 	public String getQueryCatalog() {
-		return "response by getQueryCatalog";
+		return "response by getQueryCatalog api";
 	}
 	@RequestMapping(value="/getUserList")
 	public String getUserList() {
